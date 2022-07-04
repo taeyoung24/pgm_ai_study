22d30893ad192ab5cd0584f2aa18337b50d16eba
taeyoung24 <55551459+taeyoung24@users.noreply.github.com>
2022-07-04 10:58:52 +0900

    Update main.py

--- modules/bot_system/main.py
+++ modules/bot_system/main.py
@@ -5,6 +5,6 @@ from modules.bot_system.debug_utils import build_time
 
 async def on_ready(bot: discord.Client):
     # 테스트용 빌드 문구
-    btime = build_time() + ' 빌드 테스트'
+    btime = build_time()
     await bot.change_presence(activity=discord.Game(name=btime + ' 빌드 테스트'))
     print(btime + ' 빌드 테스트가 준비되었습니다.')
